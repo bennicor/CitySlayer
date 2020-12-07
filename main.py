32e785d70857df902a2cd8f05b209a0464849218
Vlad Boldyrev <boldyrev2040@gmail.com>
2020-12-07 22:53:18 +0300

    Добавлено перемещение

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
 import pygame
 
-
+pygame.init()
 size = width, height = 500, 500
 screen = pygame.display.set_mode(size)
+
+
+class Hero:
+    def __init__(self):
+        self.vel = 5 # Скорость персонажа
+        self.width = 50
+        self.height = 50
+        self.x = width // 2
+        self.y = height - self.height
+        self.isJump = False # Проверка на прыжок
+        
+
+    def draw(self):
+        pygame.draw.rect(screen, pygame.Color("green"), (
+                self.x, self.y, self.width, self.height
+            ))
+
+    def movement(self):
+        state = pygame.key.get_pressed()
+
+        # Перемещение
+        if state[pygame.K_LEFT] and self.x > self.vel:
+            self.x -= self.vel
+        elif state[pygame.K_RIGHT] and self.x < width - self.vel - self.width:
+            self.x += self.vel
+
+        # Прыжок
+        # if not(self.isJump): # Checks is user is not jumping
+        #     if state[pygame.K_UP] and self.y > self.vel:  # Same principles apply for the y coordinate
+        #         self.y -= self.vel
+
+        #     if state[pygame.K_DOWN] and self.y < height - self.height - self.vel:
+        #         self.y += self.vel
+
+        #     if state[pygame.K_SPACE]:
+        #         self.isJump = True
+
+
 clock = pygame.time.Clock()
-x, y = 0, 0
 fps = 60
+hero = Hero()
 running = True
 
 while running:
@@ -13,6 +51,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    hero.draw()
+    hero.movement()
 
     clock.tick(fps)
     pygame.display.flip()
