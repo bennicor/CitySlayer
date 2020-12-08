bdf59621e9a81b5801b99d3fb7861ef061da3b9d
Vlad Boldyrev <boldyrev2040@gmail.com>
2020-12-08 18:16:58 +0300

    Добавлена возможность прыжка

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import pygame
 
+
 pygame.init()
-size = width, height = 500, 500
+size = WIDTH, HEIGHT = 1280, 720
 screen = pygame.display.set_mode(size)
 
 
+# Класс персонажа
 class Hero:
     def __init__(self):
-        self.vel = 5 # Скорость персонажа
+        self.vel = 3 # Скорость персонажа
         self.width = 50
         self.height = 50
-        self.x = width // 2
-        self.y = height - self.height
+        self.x = WIDTH // 2
+        self.y = HEIGHT - self.height
         self.isJump = False # Проверка на прыжок
-        
-
+        self.jumpCount = 10 # Параметр квадратичной функции для расчета траектории прыжка
+    
     def draw(self):
         pygame.draw.rect(screen, pygame.Color("green"), (
                 self.x, self.y, self.width, self.height
@@ -24,21 +26,22 @@ class Hero:
         state = pygame.key.get_pressed()
 
         # Перемещение
-        if state[pygame.K_LEFT] and self.x > self.vel:
+        if state[pygame.K_LEFT] and self.x > 0:
             self.x -= self.vel
-        elif state[pygame.K_RIGHT] and self.x < width - self.vel - self.width:
+        elif state[pygame.K_RIGHT] and self.x < WIDTH - self.width:
             self.x += self.vel
 
         # Прыжок
-        # if not(self.isJump): # Checks is user is not jumping
-        #     if state[pygame.K_UP] and self.y > self.vel:  # Same principles apply for the y coordinate
-        #         self.y -= self.vel
-
-        #     if state[pygame.K_DOWN] and self.y < height - self.height - self.vel:
-        #         self.y += self.vel
-
-        #     if state[pygame.K_SPACE]:
-        #         self.isJump = True
+        if not(self.isJump): 
+            if state[pygame.K_SPACE]:
+                self.isJump = True
+        else:
+            if self.jumpCount >= -10:
+                self.y -= (self.jumpCount * abs(self.jumpCount)) * 0.2
+                self.jumpCount -= 1
+            else: 
+                self.jumpCount = 10
+                self.isJump = False
 
 
 clock = pygame.time.Clock()
@@ -48,6 +51,7 @@ running = True
 
 while running:
     screen.fill(pygame.Color("black"))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
