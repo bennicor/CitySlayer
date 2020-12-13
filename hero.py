77c7a537d5ebf857e78c1d2f3303ae3e7dd85626
Vlad Boldyrev <boldyrev2040@gmail.com>
2020-12-13 10:09:32 +0300

    Начало работы над системой состояний

--- hero.py
+++ hero.py
@@ -1,83 +1,138 @@
 import pygame
+from pygame.math import Vector2
 
 WIDTH, HEIGHT = 1280, 720
 HERO_SPEED = 8
 GRAVITY = 1.5
 
 
+class DashState:
+    def __init__(self, velocity_x, next_state):
+        self.dash_timer = .2  # The dash will last .5 seconds.
+        self.velocity_x = velocity_x
+        self.next_state = next_state
+
+    def handle_event(self, player, event):
+        # Can queue the Move- or IdleState as the
+        # next state after the dashing.
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                self.next_state = MoveState(-HERO_SPEED)
+            elif event.key == pygame.K_d:
+                self.next_state = MoveState(HERO_SPEED)
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                self.next_state = IdleState()
+            elif event.key == pygame.K_d:
+                self.next_state = IdleState()
+
+    def update(self, player, dt, platforms=[]):
+        # Decrement the timer each frame.
+        self.dash_timer -= dt
+        # Update the position of the player.
+        player.pos.x += self.velocity_x
+        player.rect.center = player.pos
+
+        if self.dash_timer <= 0:  # Once the timer is done...
+            # switch to the queued state.
+            player.state = self.next_state
+
+
+class MoveState:
+    def __init__(self, velocity_x):
+        self.velocity_x = velocity_x
+
+    def handle_event(self, player, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                return DashState(-8, player.state)
+            elif event.key == pygame.K_e:
+                return DashState(8, player.state)
+            elif event.key == pygame.K_LEFT:
+                self.velocity_x = -HERO_SPEED
+            elif event.key == pygame.K_RIGHT:
+                self.velocity_x = HERO_SPEED
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT and self.velocity_x < 0:
+                player.temp_dir = "left"
+                return IdleState()
+            elif event.key == pygame.K_RIGHT and self.velocity_x > 0:
+                player.temp_dir = "right"
+                return IdleState()
+
+    def update(self, player, dt, platforms=[]):
+        player.vel.x = self.velocity_x
+        player.rect.x += self.velocity_x
+
+
+class IdleState:
+    def handle_event(self, player, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                return MoveState(-HERO_SPEED)
+            elif event.key == pygame.K_RIGHT:
+                return MoveState(HERO_SPEED)
+            elif event.key == pygame.K_q:
+                return DashState(-8, player.state)
+            elif event.key == pygame.K_e:
+                return DashState(8, player.state)
+
+    def update(self, player, dt, platforms=[]):
+        player.vel.x = 0
+
 # Класс персонажа
 class Hero(pygame.sprite.Sprite):
-    def __init__(self, x, y, group):
+    def __init__(self, pos, *group):
         super().__init__(group)
-        self.xvel = self.yvel = 0 # Векторы скоростей
+        self.vel = Vector2(0, 0)
+        self.pos = Vector2(pos)
         self.width = 30
-        self.height = 30
-        self.x = x
-        self.y = y
+        self.height = 50
+        self.state = IdleState()
 
         self.image = pygame.Surface((self.width, self.height))
         self.image.fill(pygame.Color("green"))
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.rect = pygame.Rect(self.pos.x, self.pos.y, self.width, self.height)
 
-        self.jumpCount = 20 # Параметр квадратичной функции для расчета траектории прыжка
+        self.jumpCount = 20
         self.onGround = False
-        self.left = self.right = False
-        
-        # Инициализация рывка
         self.temp_dir = ""
-        self.dash_speed = 15
 
-    def update(self, platforms):
-        # Перемещение по горизонтали
-        if self.left:
-            self.xvel = -HERO_SPEED
-        elif self.right:
-            self.xvel = HERO_SPEED
+    def gravitation(self):
+        # Реализация силы притяжения
+        if self.vel.y <= 12.5:
+            self.vel.y += GRAVITY
 
-        if not(self.right or self.left):
-            self.xvel = 0
-
-        self.gravitation(platforms)
+        self.rect.y += self.vel.y
 
     def collide(self, xvel, yvel, platforms):
         hits = pygame.sprite.spritecollide(self, platforms, False)
-        self.onGround = False
+        # self.onGround = False
         if hits: # если есть пересечение платформы с игроком
             if yvel > 0:
                 self.rect.bottom = hits[0].rect.top
                 self.onGround = True
-                self.yvel = 0
+                self.vel.y = 0
             
             if yvel < 0:
                 self.rect.top = hits[0].rect.bottom
-                self.yvel = 0
+                self.vel.y = 0
             
             if xvel > 0:
-                self.rect.right = hits[0].rect.left
-                self.xvel = 0
+                # self.rect.right = hits[0].rect.left
+                self.vel.x = 0
 
             if xvel < 0:
-                self.rect.left = hits[0].rect.right
-                self.xvel = 0
+                # self.rect.left = hits[0].rect.right
+                self.vel.x = 0
 
-    def gravitation(self, platforms):
-        # Реализация силы притяжения
-        if self.yvel <= 12.5:
-            self.yvel += GRAVITY
-        
-        self.rect.x += self.xvel
-        self.collide(self.xvel, 0, platforms)
+    def handle_event(self, event):
+        new_state = self.state.handle_event(self, event)
+        self.state = new_state if new_state is not None else self.state
 
-        self.rect.y += self.yvel
-        self.collide(0, self.yvel, platforms)
-
-    def jump(self):
-        if self.onGround:
-            self.yvel = -self.jumpCount
-            self.onGround = False
-
-    def dash(self):
-        if self.temp_dir == "left":
-            self.dash_speed = -self.dash_speed
-
-        self.rect.x += self.dash_speed
+    def update(self, dt, platforms):
+        self.state.update(self, dt)
+        self.gravitation()
+        print(self.vel.x, self.vel.y)
+        self.collide(self.vel.x, 0, platforms)
+        self.collide(0, self.vel.y, platforms)
--- main.py
+++ main.py
@@ -45,12 +45,12 @@ def render(level):
 
 all_sprites = pygame.sprite.Group()
 platforms = [] # объекты, с которыми будет происходить взаимодействие
-hero = Hero(300, 40, all_sprites)
+hero = Hero((120, 100), all_sprites)
 render(level)
 
 clock = pygame.time.Clock()
 fps = 60
-dash = False
+dt = 0
 running = True
 
 while running:
@@ -58,44 +58,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        hero.handle_event(event)
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and not dash:
-                hero.left = True
-                hero.temp_dir = "left"
-            elif event.key == pygame.K_RIGHT and not dash:
-                hero.right = True
-                hero.temp_dir = "right"
-            elif event.key == pygame.K_SPACE:
-                hero.jump()
-            elif event.key == pygame.K_LSHIFT and hero.temp_dir:
-                dash = True
-                timer = .2
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                hero.left = False
-            elif event.key == pygame.K_RIGHT:
-                hero.right = False
-
-    # Засекается таймер, определяющий длительность рывка
-    if dash:
-        timer -= dt
-
-        if timer > 0:
-            hero.dash_speed = 15
-            hero.dash()
-        else:
-            dash = False
-
+    all_sprites.update(dt, platforms)
     all_sprites.draw(screen)
-
-    # Во время рывка все остальные процессы останавливаются
-    if not dash:
-        hero.update(platforms)
-    else:
-        hero.yvel = 0
-        hero.collide(hero.dash_speed, 0, platforms)
 
     dt = clock.tick(fps) / 1000
     pygame.display.flip()
